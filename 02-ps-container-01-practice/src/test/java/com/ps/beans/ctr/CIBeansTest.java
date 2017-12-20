721176601b3e36c17a5a3ba855211d04004fbc00
Oksana Moiseeva <oksana_moiseeva@epam.com>
2017-12-20 15:30:30 +0300

    TODO 3 done

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
@@ -6,9 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 /**
  * Created by iuliana.cosmina on 3/26/16.
  */
@@ -24,7 +21,8 @@ public class CIBeansTest {
         for(String beanName :  ctx.getBeanDefinitionNames()) {
             logger.info("Bean " + beanName + " of type " + ctx.getBean(beanName).getClass().getSimpleName());
         }
-
-        //TODO 3. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
+        System.out.println(ctx.getBean("complexBean0"));
+        System.out.println(ctx.getBean("complexBean1"));
+        System.out.println(ctx.getBean("complexBean2"));
     }
 }
