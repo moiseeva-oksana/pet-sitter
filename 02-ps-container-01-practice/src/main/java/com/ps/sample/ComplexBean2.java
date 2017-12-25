a2da8949a0a25d4951d80660a3b0dfc36c6fa6a5
Oksana Moiseeva <oksana_moiseeva@epam.com>
2017-12-25 17:07:26 +0300

    TODO 8-12 done

--- 02-ps-container-01-practice/src/main/java/com/ps/sample/ComplexBean2.java
+++ 02-ps-container-01-practice/src/main/java/com/ps/sample/ComplexBean2.java
@@ -3,6 +3,9 @@ package com.ps.sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * Created by iuliana.cosmina on 3/28/16.
  * Sample class used to make bean creation staged understanding easy.
@@ -31,6 +34,7 @@ public class ComplexBean2 {
      * The initialization method.
      * Just for fun: it instantiates the simpleBean2 only if the current time is even.
      */
+    @PostConstruct
     private void initMethod() {
         logger.info(" --> Stage 3: Calling the initMethod.");
         long ct = System.currentTimeMillis();
@@ -42,6 +46,7 @@ public class ComplexBean2 {
     /**
      * Destroy method
      */
+    @PreDestroy
     private boolean destroyMethod() {
         logger.info(" --> Calling the destroyMethod.");
         simpleBean1 = null;
