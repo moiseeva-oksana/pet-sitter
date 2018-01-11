9eaef7ee0005ee30b88f11206771d44615b9bd9b
Oksana Moiseeva <moiseeva.ox@gmail.com>
2018-01-11 13:21:33 +0300

    TODO 29 done

--- 05-ps-jdbc-practice/src/main/java/com/ps/repos/impl/JdbcNamedTemplateUserRepo.java
+++ 05-ps-jdbc-practice/src/main/java/com/ps/repos/impl/JdbcNamedTemplateUserRepo.java
@@ -15,7 +15,6 @@ import java.util.*;
  * Created by iuliana.cosmina on 6/4/16.
  */
 @Repository("userNamedTemplateRepo")
-//TODO 29. Some of the methods have incomplete bodies that need to be completed with appropriate NamedParameterJdbcTemplate instance calls.
 public class JdbcNamedTemplateUserRepo implements UserRepo {
 
     private RowMapper<User> rowMapper = new UserRowMapper();
@@ -58,13 +57,16 @@ public class JdbcNamedTemplateUserRepo implements UserRepo {
         params.put("pass", password);
         params.put("email", email);
         String query = "insert into p_user(ID, USERNAME, PASSWORD, EMAIL) values(:id,:un,:pass, :email)";
-        // add NamedParameterJdbcTemplate instance call to create an user
+        jdbcNamedTemplate.getJdbcOperations().update(query, params);
         return 0;
     }
 
     @Override
     public int deleteById(Long userId) {
-        // add NamedParameterJdbcTemplate instance call to delete an user
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", userId);
+        String query = "delete from p_user where id = :id";
+        jdbcNamedTemplate.getJdbcOperations().update(query, params);
         return 0;
     }
 
@@ -78,7 +80,6 @@ public class JdbcNamedTemplateUserRepo implements UserRepo {
         String sql = "select id, email, username,password from p_user where id= :id";
         Map<String, Object> params = new HashMap<>();
         params.put("id", id);
-        // add NamedParameterJdbcTemplate instance call to find an user
         return null;
     }
 
